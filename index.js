701fd999854ee3c746314f06f4f68db932f6ebec
COY <ysmood@gmail.com>
2024-01-07 11:20:05 +0900

    adding mediaRoute to bundle all handlers

--- index.js
+++ index.js
@@ -1,9 +1,33 @@
+"use strict";
+
+let endfw = require('endfw'); //g is defined here
+let mediaRoute = new endfw.subroute();
+let metadata = require('./library/metadata');
+let automate = require('./library/automate');
+
+let pathHandler = {};
+g.serviceConfig.data.libraryPaths.forEach(path=>{
+  pathHandler[path] = 
+    new endfw.file.FileSegment({basePath: path}).handler;
+});
+mediaRoute.all(
+  '/mediaFile/*',
+  (req, res, next)=>{
+    handler = pathHandler[req.p.libraryPath];
+    if (!handler) return endfw.file.FileSegment.errorCallback(res, undefined, 404, "libraryPath not found");
+    return handler(req, res, next);
+  }
+);
+mediaRoute.all('/automate/*', automate.automateAPI, 'automate');
+mediaRoute.all('/metadata/*', metadata.metadataAPI, 'metadata');
+
 // for being imported as node module
 if (typeof module === 'undefined') {
   // skip if not running node
 } else {
   module.exports = {
-    metadata: require('./library/metadata'),
-    automate: require('./library/automate'),
+    mediaRoute,
+    metadata,
+    automate,
   }
 }
--- nodejs/quick/main.js
+++ nodejs/quick/main.js
@@ -33,8 +33,7 @@ mainRoute.use(endfw.server.basicParseRoute, 'basicParseRoute');
 mainRoute.use(endfw.ingest.ingestRequest.parsedUrl_p(g.server));
 mainRoute.use(endfw.ingest.logRequest.ip_method_url);
 mainRoute.use(endfw.ingest.authFree.cache_path_inventory(g.assetConfig.authFree));
-mainRoute.all('/automate/*', endmedia.automate.automateAPI, 'automate');
-mainRoute.all('/metadata/*', endmedia.metadata.metadataAPI, 'metadata');
+mainRoute.use(endmedia.mediaRoute);
 mainRoute.all('/web/css/*', endfw.lessCss({
   contextPath: '/web/css',
   pathFromReq: req=>req.parsedUrl.remainingPath(),
